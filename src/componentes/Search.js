0ee1dd6d3acb675355edf7df4754a4d6d92da27e
allan0912 <allanheller4@gmail.com>
2023-12-26 17:09:53 -0500

    Se agrego funcionalidad para un  header fijo

--- src/componentes/Search.js
+++ src/componentes/Search.js
@@ -1,16 +1,29 @@
-import React, { useState} from "react";
+import React, { useEffect, useState} from "react";
 import '../estilos/Estilos-search.css'
 
 
 
 function Search({onChange, onSubmit}){
+    
+        const [fixed, setFixed] = useState(false)
+        useEffect(()=>{
+            const handleScroll = () =>{
+              const isScrolled = window.scrollY > 0
+                setFixed(isScrolled)
+            }
+            window.addEventListener('scroll', handleScroll)
+            return () =>{
+                window.removeEventListener('scroll',handleScroll)
+            }
+        }, [])
+    
     const [mostrarInput, setMostrarInput] = useState(false)   
     const salirInput = () =>{
             setMostrarInput(!mostrarInput)
     }
     return(
         <form  onSubmit={onSubmit}>
-            <header className="header flex items-center justify-center flex-col">
+            <header className={`menu${fixed ? ' fixed' : ''}`}>
             <nav className="navegation" >
                 <div className="search">
                     <li className="list-none mt-5 mb-6"><i  onClick={salirInput}  className='icon-search bx bx-search text-6xl text-orange-50 cursor-pointer'></i></li>                
